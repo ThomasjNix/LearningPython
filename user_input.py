0866bf2a4124010c3a3cf3d59f3e822ff0326914
Thomas Nix <tnix2@uncc.edu>
2020-06-29 21:04:23 -0400

    User input

---
+++ user_input.py
@@ -0,0 +1,3 @@
+# Prompt the user for an input and store the entered value in a variable
+users_favorite_color = input("What is your favorite color?\n")
+print("You said your favorite color was " + users_favorite_color)
