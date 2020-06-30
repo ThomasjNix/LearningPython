5867ca1406076b72999648db6b4aef6022d88c69
Thomas Nix <tnix2@uncc.edu>
2020-06-29 21:45:45 -0400

    Tuples example

---
+++ PythonBasics/tuples.py
@@ -0,0 +1,8 @@
+# Tuples are created by specifying a set of data with parenthesis
+example_tuple = (1,3)
+
+# Tuples are immutable
+# example_tuple = (4,5) - Not valid!
+
+# Tuples are index starting at 0
+print(example_tuple[0]) # 1
