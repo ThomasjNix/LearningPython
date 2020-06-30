4292783945e98555046d35d50f21466656f8a065
Thomas Nix <tnix2@uncc.edu>
2020-06-29 21:11:10 -0400

    New project file - user_input_math_output

---
+++ Projects/user_input_math_output.py
@@ -0,0 +1,9 @@
+num1 = float(input("What is the first number?\n"))
+num2 = float(input("What is the second number?\n"))
+
+print(str(num1) + " + " + str(num2) + " = " + str(num1 + num2))
+print(str(num1) + " - " + str(num2) + " = " + str(num1 - num2))
+print(str(num1) + " / " + str(num2) + " = " + str(num1 / num2))
+print(str(num1) + " * " + str(num2) + " = " + str(num1 * num2))
+print(str(num1) + " ^ " + str(num2) + " = " + str(num1 ** num2))
+print(str(num1) + " % " + str(num2) + " = " + str(num1 % num2))
