e8bd8a35d72513fb09993d1f47b66e220b3bd78e
Thomas Nix <tnix2@uncc.edu>
2020-06-30 19:29:39 -0400

    Throw error as error for better logging

--- PythonBasics/try_except.py
+++ PythonBasics/try_except.py
@@ -4,11 +4,13 @@ try:
     print("Good job, you entered an integer. It was " + str(userInput))
     print("Now I'm going to divide by zero to show error handling with finer control")
     print(userInput / 0)
-except ZeroDivisionError:
+except ZeroDivisionError as err:
     print("Hey you can't just go around dividing by zero like that!")
-except:
+    print("Actual error encountered: " + str(err))
+except ValueError as err:
     # This is a ValueError, but it's being used as a catch-all
     print("Entering an integer was a simple request but you couldn't even get that right.")
+    print("Actual error encountered: " + str(err))
 
 
 '''
