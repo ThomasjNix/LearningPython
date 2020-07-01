a44e3b3490b3227f1ceb0fffe188f8548bbdb88c
Thomas Nix <tnix2@uncc.edu>
2020-07-01 07:10:59 -0400

    Example of inheritance

--- PythonBasics/student.py
+++ PythonBasics/student.py
@@ -1,11 +1,19 @@
+from person import Person
+
+# Inheritance makes it possible to extend existing functionality for finer organization of class structures
+
 """
 Classes are defined with this syntax
 the initializer function is used when constructing an instance of the class
 all arguments beyond self are passed and can be used for assignment
+
+A standard class is just class ClassName(), but a class to extend (in this case person) can be provided
+When extending properties from a base class, be sure to make the super() call and call __init__() with the
+necessary parameters needed to initialize the instance of the base class
 """
-class Student:
+class Student(Person):
     def __init__(self, name, major, gpa):
-        self.name = name
+        super().__init__(name)
         self.major = major
         self.gpa = gpa
 
